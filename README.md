c468374452d824840c17c686e7c54ead443960f7
Justice Eziefule <eziefulejustice@gmail.com>
2019-10-10 16:09:24 +0100

    first push

---
+++ README.md
@@ -0,0 +1,12 @@
+# simple_flutter_app
+
+A new Flutter project.
+
+## Getting Started
+
+This project is done as the basic steps in creating a flutter app.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import './quiz.dart';
+import './result.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favourite club?',
+      'answers': [{'text': 'Black', 'score': 10}, {'text': 'Red', 'score': 6}, {'text': 'Green', 'score': 2}, {'text': 'White', 'score': 30}],
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
+      'answers': [{'text': 'Rabbit', 'score': 10}, {'text': 'Leopard', 'score': 7}, {'text': 'Elephant', 'score': 1}, {'text': 'Lion', 'score': 2}],
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [{'text': 'Max', 'score': 2}, {'text': 'Colt', 'score': 10}, {'text': 'Angela', 'score': 7}, {'text': 'Justice', 'score': 10}],
+    },
+  ];
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+    _questionIndex = 0;
+    _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+      if (_questionIndex < _questions.length) {
+        print('We have more questions!');
+      } else {
+        print("no more questions");
+      }
+    });
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
+      ),
+    );
+  }
+}
